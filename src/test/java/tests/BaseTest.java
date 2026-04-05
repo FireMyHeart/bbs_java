e1e1d2a27e5faee678665a28ad4a7ce15c8b9d7c
FireMyHeart <nyutyan@gmail.com>
2026-04-05 18:26:56 +0300

    added BaseTest and incorrect login test

---
+++ src/test/java/tests/BaseTest.java
@@ -0,0 +1,25 @@
+package tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        // System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        options.addArguments("headless");
+        driver = new ChromeDriver(options);
+    }
+
+    @AfterMethod
+    public void close() {
+        driver.quit();
+    }
+}
