26f278a874896ffa6aa5ddad150199f83c549c4b
FireMyHeart <nyutyan@gmail.com>
2026-04-06 11:30:28 +0300

    updated test login

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -13,7 +13,7 @@ public class LoginTest extends BaseTest {
     @Test
     public void checkLogin() {
         driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_userjhksdckczc");
+        driver.findElement(By.id("user-name")).sendKeys("standard_case");
         driver.findElement(By.id("user-name")).sendKeys(Keys.CONTROL + "A");
         driver.findElement(By.id("user-name")).sendKeys(Keys.BACK_SPACE);
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
@@ -26,7 +26,7 @@ public class LoginTest extends BaseTest {
     @Test
     public void checkIncorrectLogin() {
         driver.get("https://www.saucedemo.com/");
-        driver.findElement(By.id("user-name")).sendKeys("standard_userjhksdckczc");
+        driver.findElement(By.id("user-name")).sendKeys("standard_test_test");
         driver.findElement(By.xpath("//*[@placeholder='Password']")).sendKeys("secret_sauce");
         driver.findElement(By.cssSelector("[data-test='login-button']")).click();
         boolean isErrorMsgDisplayed = driver.findElement(By.cssSelector("h3[data-test='error']")).isDisplayed();
