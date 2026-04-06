1fed614f4610e5776f93678cd6296e3818ee16df
FireMyHeart <nyutyan@gmail.com>
2026-04-06 19:38:58 +0300

    removed backspace from DeclareVariables

--- src/main/java/DeclareVariables.java
+++ src/main/java/DeclareVariables.java
@@ -8,7 +8,7 @@ public class DeclareVariables {
 
     public String declareVariables() {
         String response = "";
-        response = "Dear " + customerName + ", thank you for supporting our business! \n"
+        response = "Dear " + customerName + ", thank you for supporting our business!\n"
                 + "Cups of coffee bought: " + cupsOfCoffee + ".\n"
                 + "Cost of 1 cup of coffee: $" + costOfCoffee + ".\n"
                 + "Your total is: $" + totalCost + ".\n"
@@ -16,16 +16,9 @@ public class DeclareVariables {
         return response;
     }
 
+    // Вот эту часть у AI спрашивала, как мне сделать аналог вызова метода и принт, как в питоне, чтобы проверить, правильно ли пишется
     public static void main(String[] args) {
         DeclareVariables d = new DeclareVariables();
         System.out.println(d.declareVariables());
     }
 }
-
-/*
-Вот эту часть у AI спрашивала, как мне сделать аналог вызова метода и принт, как в питоне, чтобы проверить, правильно ли пишется:
-    public static void main(String[] args) {
-        DeclareVariables d = new DeclareVariables();
-        System.out.println(d.declareVariables());
-    }
- */
