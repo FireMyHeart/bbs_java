c20637dd66cc2ecbf1213eec485715f099aecd4c
FireMyHeart <nyutyan@gmail.com>
2026-04-07 20:43:21 +0300

    created products page and login page classes

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,9 +5,15 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import pages.LoginPage;
+import pages.ProductsPage;
+
+import java.time.Duration;
 
 public class BaseTest {
     public WebDriver driver;
+    LoginPage loginPage;
+    ProductsPage productsPage;
 
     @BeforeMethod
     public void setup() {
@@ -16,6 +22,9 @@ public class BaseTest {
         options.addArguments("start-maximized");
         options.addArguments("headless");
         driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        loginPage = new LoginPage(driver);
+        productsPage = new ProductsPage(driver);
     }
 
     @AfterMethod
