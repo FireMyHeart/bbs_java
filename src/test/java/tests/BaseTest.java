3678db6bfa387f675fecec3b64976f9c6a83d6c2
FireMyHeart <nyutyan@gmail.com>
2026-04-16 09:34:39 +0300

    removed comment line in headless mode

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -24,7 +24,7 @@ public class BaseTest {
         // System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("start-maximized");
-        // options.addArguments("headless");
+        options.addArguments("headless");
         driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         loginPage = new LoginPage(driver);
