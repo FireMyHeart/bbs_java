de1d49855a1e2cdb8985df04731acd42c0b69efb
FireMyHeart <nyutyan@gmail.com>
2026-04-29 00:38:07 +0300

    fixed

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -30,7 +30,7 @@ public class BaseTest {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("start-maximized");
             options.addArguments("guest");
-            // options.addArguments("headless");
+            options.addArguments("headless");
             driver = new ChromeDriver(options);
             driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         } else if (browser.equalsIgnoreCase("firefox")) {
